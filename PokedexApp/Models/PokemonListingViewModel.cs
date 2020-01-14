a16750f20b2afa85d5efda2ed4214f81addf0ed1
Stanco, Robyn <Robyn.Stanco@financialpartners.com>
2020-01-14 13:29:10 -0500

    image updates

--- PokedexApp/Models/PokemonListingViewModel.cs
+++ PokedexApp/Models/PokemonListingViewModel.cs
@@ -5,12 +5,6 @@ namespace PokedexApp.Models
     public class PokemonListingViewModel
     {
         public string ImageURL { get; set; }
-        public string ImageSource {
-            get
-            {
-                return ImageURL != null ? string.Format("data:image/png;base64,{0}", ImageURL) : null;
-            }
-        }
         public Guid? MyPokemonId { get; set; }
         public string Name { get; set; }
         public int? NationalDexPokemonId { get; set; }
