a893f2cd0fae3c3c2ff31a8ecc8d9459e2cb57a4
Robyn Stanco <robyn.stanco@gmail.com>
2020-09-02 20:54:37 -0400

    removed api for now, updated connection string

--- Pokedex.Data/POKEDEXDBContext.cs
+++ Pokedex.Data/POKEDEXDBContext.cs
@@ -25,11 +25,7 @@ namespace Pokedex.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-OTPCPIS;initial catalog=POKEDEXDB;integrated security=True;");
-            }
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-VP213OT;initial catalog=POKEDEXDB;integrated security=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
