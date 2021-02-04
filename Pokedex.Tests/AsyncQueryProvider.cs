3244c6409d9cd0f50c569ba4de0582f45f49516f
Robyn Stanco <robyn.stanco@gmail.com>
2021-02-04 18:11:47 -0500

    updated refrence to ef core for tests (IAsyncQueryProvider)

--- Pokedex.Tests/AsyncQueryProvider.cs
+++ Pokedex.Tests/AsyncQueryProvider.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+﻿using Microsoft.EntityFrameworkCore.Query;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
