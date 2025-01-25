b8ab8604a585049f977481972f5058970f3d9199
Robyn Stanco <robyn.stanco@gmail.com>
2025-01-24 22:13:57 -0500

    update comments

--- Pokedex.Common/Helpers/PaginationHelper.cs
+++ Pokedex.Common/Helpers/PaginationHelper.cs
@@ -24,8 +24,8 @@ namespace Pokedex.Common.Helpers
         /// </summary>
         /// <typeparam name="T">The class of the data.</typeparam>
         /// <param name="collection">The enumerable collection of type T.</param>
-        /// <param name="pageNumber">The current page number to take.</param>
-        /// <param name="pageSize">The page size to take.</param>
+        /// <param name="pageNumber">The current page number.</param>
+        /// <param name="pageSize">The page size.</param>
         /// <returns>A PagedResult of class T.</returns>
         public PagedResult<T> GetPagedResults<T>(IEnumerable<T> collection, int pageNumber, int pageSize) where T : class
         {
--- Pokedex.Common/Interfaces/IPaginationHelper.cs
+++ Pokedex.Common/Interfaces/IPaginationHelper.cs
@@ -14,8 +14,8 @@ namespace Pokedex.Common.Interfaces
         /// </summary>
         /// <typeparam name="T">The class of the data.</typeparam>
         /// <param name="collection">The enumerable collection of type T.</param>
-        /// <param name="pageNumber">The current page number to take.</param>
-        /// <param name="pageSize">The page size to take.</param>
+        /// <param name="pageNumber">The current page number.</param>
+        /// <param name="pageSize">The page size.</param>
         /// <returns>A PagedResult of class T.</returns>
         PagedResult<T> GetPagedResults<T>(IEnumerable<T> collection, int pageNumber, int pageSize) where T : class;
     }
